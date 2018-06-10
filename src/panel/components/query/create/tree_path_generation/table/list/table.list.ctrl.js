58a86e41acc17a2852706e47b05df1e6b7993c93
Zed25 <2simonemancini5@gmail.com>
2018-06-10 18:33:27 +0200

    query tree path generated where clauses view loop

--- src/panel/components/query/create/tree_path_generation/table/list/table.list.ctrl.js
+++ src/panel/components/query/create/tree_path_generation/table/list/table.list.ctrl.js
@@ -1,7 +1,9 @@
-angular.module('app.ctrl').controller('queryTableListController', function (userService, queryService, queryBuilderService, $timeout) {
+angular.module('app.ctrl').controller('queryTableListController', function (userService, queryService, queryBuilderService, $timeout, $routeParams) {
     const self = this;
 
     self.tables = [];
+
+    self.filteredTables = [];
 
     self.generalColumn = {
         name: "General Query (select this line to perform query without where clauses)",
@@ -9,6 +11,12 @@ angular.module('app.ctrl').controller('queryTableListController', function (user
     };
 
     self.queryText = "SELECT count(*) FROM ";
+
+    // select table feature flag
+    self.selectable = true;
+
+    // append modality feature flag
+    self.appendMood = false;
 
     self.getTablesMetadata = function () {
         if (!userService.isLogged()) {
@@ -22,6 +30,7 @@ angular.module('app.ctrl').controller('queryTableListController', function (user
 
         queryService.getTablesMetadata(function (response) {
             self.tables = response.data;
+            [].push.apply(self.filteredTables, self.tables);
         }, function () {
             alert("Invalid tables metadata");
         });
@@ -38,20 +47,32 @@ angular.module('app.ctrl').controller('queryTableListController', function (user
 
     self.selectTable = function(table) {
 
-        //if table is already selected, deselect it and return
-        if (table.selected) {
-            table.selected = false;
-            return;
+        //select table only if selectable flag is true
+        if (self.selectable) {
+
+            //if table is already selected, deselect it and return
+            if (table.selected) {
+                table.selected = false;
+
+                //restore tables
+                self.filteredTables = [];
+                [].push.apply(self.filteredTables, self.tables);
+                return;
+            }
+
+            angular.forEach(self.tables, function (t) {
+
+                t.selected = false;
+
+            });
+
+
+            table.selected = true;
+
+            //filter tables
+            self.filteredTables = [table];
+
         }
-
-        angular.forEach(self.tables, function (t) {
-
-            t.selected = false;
-
-        });
-
-
-        table.selected = true;
 
     };
 
@@ -99,13 +120,33 @@ angular.module('app.ctrl').controller('queryTableListController', function (user
 
             self.updateQueryText(table, column);
 
-            window.location.href = "#/query/create/tree_path/where_clauses";
+            window.location.href = "#/query/create/tree_path/where_clauses/" + table.name + "/" + column.name;
 
-        };
+        }
 
     };
 
     self.updateQueryText = function(table, column) {
+
+        // use append feature
+        if (self.appendMood) {
+
+            var q = queryBuilderService.get();
+
+            //check column != null
+            if (column == null) {
+
+                alert("no column selected!")
+
+                //TODO do something
+            }
+
+            q.queryText += column.name + " ";
+
+            queryBuilderService.set(q);
+
+            return;
+        }
 
         //update query text
         self.queryText += table.name;
@@ -155,6 +196,34 @@ angular.module('app.ctrl').controller('queryTableListController', function (user
 
         queryService.getTablesMetadata(function (response) {
             self.tables = response.data;
+
+            if ($routeParams.table === "0") {
+
+                [].push.apply(self.filteredTables, self.tables);
+
+            } else {
+
+                var table = self.tables.filter(function( t ) {
+                    return t.name === $routeParams.table;
+                });
+
+                if (table == null) {
+                    alert("Error: try again from beginning");
+
+                    //redirect to query path beginning
+                    window.location.href = "/query/create/tree_path/type";
+                }
+
+                self.selectTable(table[0]);
+
+                //single table mood
+                self.selectable = false;
+
+                //append modality
+                self.appendMood = true;
+
+            }
+
             $timeout(function () {
                 M.AutoInit();
             });
--- src/panel/components/query/create/tree_path_generation/where_clauses/where.clauses.ctrl.js
+++ src/panel/components/query/create/tree_path_generation/where_clauses/where.clauses.ctrl.js
@@ -1,5 +1,135 @@
 'use strict';
 
-angular.module('app.ctrl').controller('queryWhereClausesController', function (userService, queryService, $timeout) {
+angular.module('app.ctrl').controller('queryWhereClausesController', function (userService, queryService, queryBuilderService, $timeout, $routeParams) {
+
+    const self = this;
+
+    self.whereClauseForm = {
+        comparisonSign : '',
+        comparisonValue : ''
+    };
+
+    self.tableName = $routeParams.table;
+
+    self.columnName = $routeParams.column;
+
+    self.queryText = "";
+
+
+    self.updateQueryText = function(whereClauseForm, modality){
+
+        var q = queryBuilderService.get();
+
+        self.queryText = q.queryText;
+
+        if(whereClauseForm.comparisonValue === "null") {
+
+            if (whereClauseForm.comparisonSign === "!=") {
+
+                self.queryText += "IS NOT NULL";
+
+            } else if (whereClauseForm.comparisonSign === "=") {
+
+                self.queryText += "IS NULL";
+
+            }
+
+        } else if(whereClauseForm.comparisonValue === "false"){
+
+            self.queryText += whereClauseForm.comparisonSign + " FALSE";
+
+        } else if(whereClauseForm.comparisonValue === "true" ){
+
+            self.queryText += whereClauseForm.comparisonSign + "TRUE";
+
+        } else if(whereClauseForm.comparisonSign === "LIKE") {
+
+            self.queryText += whereClauseForm.comparisonSign + " '" + whereClauseForm.comparisonValue + "%'";
+
+        } else {
+
+            self.queryText += whereClauseForm.comparisonSign + " '" + whereClauseForm.comparisonValue + "'";
+
+        }
+
+        switch (modality) {
+
+            case "and":
+
+                self.queryText += " AND ";
+
+                break;
+
+            case "or":
+
+                self.queryText += " AND ";
+
+                break;
+
+            default:
+
+                self.queryText += ";";
+
+                break;
+
+        }
+
+        q.queryText = self.queryText;
+
+        queryBuilderService.set(q);
+
+    };
+
+    self.goNext = function(modality) {
+
+        //validate form
+        if (!self.validateForm(self.whereClauseForm)) {
+
+            alert("Where clause not complete, please insert both comparison sign and comparison value!")
+
+            return;
+
+        }
+
+        self.updateQueryText(self.whereClauseForm, modality);
+
+        switch (modality) {
+
+            case "and":
+            case "or":
+
+                window.location.href = "#/query/create/tree_path/table/list/" + self.tableName;
+
+                break;
+
+            default:
+
+                window.location.href = "#/query/create/tree_path/create";
+
+                break;
+        }
+
+    };
+
+    self.validateForm = function(whereClauseForm) {
+
+        return whereClauseForm.comparisonSign != null
+                && whereClauseForm.comparisonSign !== ""
+                && whereClauseForm.comparisonValue != null
+                && whereClauseForm.comparisonValue !== "";
+
+    };
+
+
+    self.init = function () {
+
+        if (!userService.isLogged()) window.location.href = "../";
+
+        $timeout(function () {
+            M.AutoInit();
+        });
+
+    }();
+
 
 });
