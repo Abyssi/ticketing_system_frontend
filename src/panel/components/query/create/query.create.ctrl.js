dba10a2b6652f806c362d39b85e4a60cce68b126
Zed25 <2simonemancini5@gmail.com>
2018-06-11 17:02:39 +0200

    query layout improved

--- src/panel/components/query/create/query.create.ctrl.js
+++ src/panel/components/query/create/query.create.ctrl.js
@@ -3,6 +3,13 @@
 angular.module('app.ctrl').controller('queryCreateController', function (userService, $timeout) {
     const self = this;
 
+    self.toQueryCreation = function(href) {
+
+        //go to next step
+        window.location.href = href;
+
+    };
+
     self.init = function () {
 
         if (!userService.isLogged()) window.location.href = "../";
